e05e62b42a2b80ee904dfbd5804c115c3a2fd921
Patrick Crowley <patrick@mokolabs.com>
2009-10-19 22:39:34 -0700

    improved behavior of code block enlarger

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,31 @@
 // Let's go
 $(document).ready(function() {
 
+  // Wrap code in spans to grab block length
   $(".highlight").wrapInner("<span></span>");
 
-  $(".highlight").hover(function() {
-      var contentwidth = $(this).contents().width();
-      var blockwidth = $(this).width();    
-      if(contentwidth > blockwidth) {
-        $(this).animate({ width: "1020px"}, 450);
-        }
-      }, function() {
-        $(this).animate({ width: "680px" }, 450);
+  // Expand long code blocks on hover
+  $(".highlight").hover(
+    function() {
+      var block = $(this).width();
+      var content = $(this).contents().width();
+      if (content > block && content < 1020) {
+        $(this).animate({ width: content + "px"}, 350);
+      }
+    },
+    function() {
+      var content = $(this).contents().width();
+      if (content > 680 && content < 1020) {
+        $(this).animate({ width: "680px" }, 350);
+      }
+    }
+  );
+  
+  // Add scrollbar to super long code blocks
+  $(".highlight").each(function(){
+    if ($(this).contents().width() > 1020) {
+      $(this).css("overflow-x", "scroll");
+    }
   });
 
 });
