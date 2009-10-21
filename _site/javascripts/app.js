72e38d136ee1635725d5ce3a4e4fb724091bc117
Patrick Crowley <patrick@mokolabs.com>
2009-10-20 17:51:32 -0700

    move along

--- _site/javascripts/app.js
+++ _site/javascripts/app.js
@@ -5,22 +5,26 @@ $(document).ready(function() {
   $(".highlight").wrapInner("<span></span>");
 
   // Expand long code blocks on hover
-  $(".highlight").hover(
-    function() {
-      var block = $(this).width();
-      var content = $(this).contents().width();
-      if (content > block && content < 1020) {
-        $(this).animate({ width: content + "px"}, 350);
-      }
-    },
-    function() {
-      var content = $(this).contents().width();
-      if (content > 680 && content < 1020) {
-        $(this).animate({ width: "680px" }, 350);
-      }
+  $(".highlight").each(function(){
+    if ($(this).contents().width() > 680) {
+      $(this).hover(
+        function() {
+          var block = $(this).width();
+          var content = $(this).contents().width();
+          if (content > block && content < 1020) {
+            $(this).animate({ width: content + "px"}, 350);
+          }
+        },
+        function() {
+          var content = $(this).contents().width();
+          if (content > 680 && content < 1020) {
+            $(this).animate({ width: "680px" }, 350);
+          }
+        }
+      );
     }
-  );
-  
+  });
+
   // Add scrollbar to super long code blocks
   $(".highlight").each(function(){
     if ($(this).contents().width() > 1020) {
