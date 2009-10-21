cbd63a81d4ad3d80a1e88d855798ee699a8dc94d
Patrick Crowley <patrick@mokolabs.com>
2009-10-21 09:46:48 -0700

    tweaked column widths on layout

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
       var content = $(this).contents().width();
       $(this).hover(
         function() {
-          if (content > block && content < 1020) {
+          if (content > block && content < 1000) {
             $(this).animate({ width: content + "px"}, 350);
           }
         },
         function() {
-          if (content > 680 && content < 1020) {
+          if (content > 680 && content < 1000) {
             $(this).animate({ width: "680px" }, 450);
           }
         }
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
   // Add scrollbar to super long code blocks
   $(".highlight").each(function(){
-    if ($(this).contents().width() > 1020) {
+    if ($(this).contents().width() > 1000) {
       $(this).css("overflow-x", "scroll");
     }
   });
