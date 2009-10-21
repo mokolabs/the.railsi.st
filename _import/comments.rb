0a18831e72354d3ce42a9815e0a12c5fe2f48859
Patrick Crowley <patrick@mokolabs.com>
2009-10-21 16:31:37 -0700

    added some notes for the mephisto comments importer

--- _import/comments.rb
+++ _import/comments.rb
@@ -3,7 +3,24 @@ require 'sequel'
 require 'fastercsv'
 require 'fileutils'
 
+# This code can be used to export comments from a single Mephisto blog post.
+
+# INSTRUCTIONS
+
+# 1. Place this file in _import within your Jekyll app directory
+# 2. Update your db credentials below (:user, :pass, etc.)
+# 3. Open the Mephisto admin interface.
+# 4. Navigate to the article you want to grab comments from
+# 5. Copy the numeric id from the query strong.
+#     (Yes... there are better ways of doing this. Use them if necessary.)
+# 6. Replace the article_id value in the QUERY below with the numeric id you copied.
+# 7. Open up a Terminal and navigate to your Jekyll app directory
+# 8  ruby comments.rb
+# 9  Copy the resulting HTML block.
+# 10  Place into a file of your choice.
+
 # Simple format comment bodies
+# (I've disabled this, but just uncomment to reactivate)
 def simple_format(text, html_options={})
  # text = text.to_s.dup
  # text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
@@ -27,7 +44,7 @@ if post[:author_url] != ''
   post[:author] = '<a href="' + post[:author_url] + '">' + post[:author] + '</a>'
 end
   
-# Format comment
+# Format comment (tweak as you see fit)
 comment = <<eos
 <!-- Comment -->
 <div class="comment">
@@ -41,10 +58,12 @@ comment = <<eos
 
 eos
 
-buffer = buffer + comment
+# Save comment to output buffer
+output = output + comment
 
 end
 
+# Export all comments for this record
 puts "<h2>Comments</h2>"
 puts
-puts buffer
+puts output
