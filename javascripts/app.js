3ba4a72a8384c42d4d9c1cc180c088a096e29255
Patrick Crowley <patrick@mokolabs.com>
2009-10-22 11:18:18 -0700

    fixed FF bug with code block expander; refactored code block expander

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,34 +1,38 @@
 // Let's go
 $(document).ready(function() {
 
-  // Wrap code in spans to grab block length
-  $(".highlight").wrapInner("<span></span>");
-
-  // Expand long code blocks on hover
-  $(".highlight").each(function(){
-    if ($(this).contents().width() > 680) {
-      var block = $(this).width();
-      var content = $(this).contents().width();
+  $(".highlight").each(function() {
+  
+    // Wrap .highlight pre blocks in span tags
+    // (so we can grab the true width of code blocks)
+    $(this).children().wrapInner("<span></span>");
+  
+    // Grab page width
+    var page_width = $("#content").width() - 20;
+    
+    // Grab .highlight width
+    var parent_width = $(this).width();
+    
+    // Grab width of code block
+    var code_width = $(this).contents().contents().width();
+    
+    // Expand/shrink code blocks
+    if (code_width > parent_width && code_width < page_width) {
       $(this).hover(
         function() {
-          if (content > block && content < 1000) {
-            $(this).animate({ width: content + "px"}, 350);
-          }
+          $(this).animate({ width: code_width + "px"}, 350);
         },
         function() {
-          if (content > 680 && content < 1000) {
-            $(this).animate({ width: "680px" }, 450);
-          }
+          $(this).animate({ width: parent_width + "px" }, 450);
         }
       );
     }
-  });
-
-  // Add scrollbar to super long code blocks
-  $(".highlight").each(function(){
-    if ($(this).contents().width() > 1000) {
+    
+    // Show horizontal scrollbar on super long code blocks
+    if (code_width > page_width) {
       $(this).css("overflow-x", "scroll");
     }
+  
   });
 
 });
